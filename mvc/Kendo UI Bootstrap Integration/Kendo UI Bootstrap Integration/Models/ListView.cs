79c64d9fd70850013f4998f40f4f1637b894d1b8
KirilNN <kiril.nikolov@gmail.com>
2015-04-17 16:45:09 +0300

    Add Scheduler, Grid, PanelBar, Menu

---
+++ mvc/Kendo UI Bootstrap Integration/Kendo UI Bootstrap Integration/Models/ListView.cs
@@ -0,0 +1,24 @@
+﻿namespace Kendo_UI_Bootstrap_Integration
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ListViewModel
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Title
+        {
+            get;
+            set;
+        }
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
